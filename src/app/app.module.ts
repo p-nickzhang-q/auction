ae6cbb3770863346c1745825382be9518bcbd20d
zhao <zhao@yicksolutions.com>
2018-10-29 16:31:11 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [//组件，指令，管道申明在declarations
+    AppComponent, NavbarComponent, FooterComponent, SearchComponent, CarouselComponent, ProductComponent, StarsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [],//服务申明在providers
   bootstrap: [AppComponent]
 })
 export class AppModule { }
