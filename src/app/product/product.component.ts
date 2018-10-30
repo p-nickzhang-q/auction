53deafe705f4f7bf8fd6c114cfea287bc519a8f5
zhao <zhao@yicksolutions.com>
2018-10-30 10:36:20 +0800

    update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  private products: Array<Product>;
+
   constructor() { }
 
   ngOnInit() {
+    this.products = [];
+    for (let i = 0; i < 5; i++) {
+      this.products.push(new Product(i + 1, 'title' + i, 100 + i, i + 0.5, 'desc' + i, ['test', 'test2']))
+    }
   }
 
+}
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ) {
+
+  }
 }
